d0951f6768913a1b12eecdfe5fc89f159c289c77
Siddhu <arevindh@gmail.com>
2018-07-05 01:29:34 +0530

    basic testing config

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthenticationService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private cookieService: CookieService) { }
+
+    token = localStorage.getItem('currentUser');
+    cookieValue: any;
 
     login(username: string, password: string) {
-        return this.http.post<any>('/api-auth/login/', { username: username, password: password })
+        return this.http.post<any>('/api-auth/login/', { username: username, password: password }, this.authHeader())
             .map(user => {
                 // login successful if there's a jwt token in the response
                 if (user && user.key) {
@@ -27,5 +31,16 @@ export class AuthenticationService {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
     }
+
+    authHeader() {
+        this.cookieValue = this.cookieService.get('csrftoken');
+        return {
+          headers: new HttpHeaders({
+            'X-CSRFToken': this.cookieValue,
+            'Authorization': `Token ${this.token}`
+          })
+        };
+      }
+    
 
 }
